70c1e4843fa2ab250e4dadfe53960cc96fce0332
CodeDDAGGI <qudrb1132@naver.com>
2025-01-04 01:49:51 +0900

    first commit

---
+++ MovieReviewPlatform/src/main/java/com/movie/moviereviewplatform/movie/MovieRepository.java
@@ -0,0 +1,4 @@
+package com.movie.moviereviewplatform.movie;
+
+public class MovieRepository {
+}
---
+++ MovieReviewPlatform/src/test/java/com/movie/moviereviewplatform/MovieReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.moviereviewplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
